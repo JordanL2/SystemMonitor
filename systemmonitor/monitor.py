e061eb01c1e87246370232c4c7968c88790f1cdc
Jordan Leppert <jordanleppert@gmail.com>
2020-11-09 11:03:30 +0000

    Initial commit

---
+++ systemmonitor/monitor.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python3
+
+from datetime import *
+import json
+import mariadb
+import re
+import subprocess
+
+
+db_user = 'monitor_insert'
+db_pass = 'qwerty'
+db_host = 'localhost'
+
+shares = [
+    '/data/Download',
+    '/data/FlatpakRepo',
+    '/data/GitRepo',
+    '/data/Jordan',
+    '/data/Margaret',
+    '/data/Multimedia',
+    '/data/Public',
+]
+
+
+def main():
+    # Get timestamp
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(now)
+
+
+    #
+    # GATHER DATA
+    #
+    
+    data = dict()
+
+
+    ### HARDWARE ###
+
+    # Memory
+    data_memory_usage(data)
+
+    # CPU Utilisation
+    data_cpu_utilisation(data)
+
+    # Disk Usage
+    disk_devices = get_disk_devices()
+    print("Found disk devices:", disk_devices)
+    for device in disk_devices:
+        data_disk_usage(data, device)
+
+    # Disk SMART Info
+    for device in disk_devices:
+        data_disk_smart(data, device['name'])
+
+    # IPMI Info
+    data_ipmi(data)
+
+
+    ### SHARES ###
+
+    # Share Size
+    # Commented out as this takes too long to calculate
+    #for share in shares:
+    #    data_share_size(data, share)
+
+    # Share last synced
+    for share in shares:
+        data_share_lastsync(data, share)
+
+
+    #
+    # INSERT DATA INTO DB
+    #
+
+    # Get connection
+    try:
+        conn = mariadb.connect(
+            user = db_user,
+            password = db_pass,
+            host = db_host,
+            database = 'monitor'
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting: {e}")
+        sys.exit(1)
+
+    conn.autocommit = False
+    cur = conn.cursor()
+
+    # Insert each row into the DB
+    for key, value in data.items():
+        unit = None
+        if len(value) == 3:
+            unit = value[2]
+        try:
+            cur.execute("INSERT INTO measurements (taken, measurement, value_type, value, unit) VALUES (?, ?, ?, ?, ?)", (now, key, value[1], str(value[0]), unit))
+        except mariadb.Error as e:
+            print(f"Error inserting data: {e}")
+            conn.close()
+            sys.exit(1)
+
+    # Commit and close
+    conn.commit()
+    conn.close()
+
+
+### DATA FETCHING ###
+
+def data_memory_usage(data):
+    result = cmd('free -wb | head -n2 | tail -n+2')
+    line_regex = re.compile('^Mem:\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s*$')
+    line_match = line_regex.match(result)
+    if line_match:
+        field_names = ('total', 'used', 'free', 'shared', 'buffers', 'cache', 'available')
+        total = int(line_match.group(1))
+        for i in range(1, len(field_names)):
+            data["hardware.memory.{0}".format(field_names[i])] = (int(line_match.group(i + 1)) / total * 100, '%')
+
+def data_cpu_utilisation(data):
+    clock_ticks_per_second = float(cmd('getconf CLK_TCK'))
+    with open('/proc/stat') as f:
+        raw = f.read()
+        line_regex = re.compile('^cpu(\d*)\s+(.+)')
+        for line in raw.split("\n"):
+            line_match = line_regex.match(line)
+            if line_match:
+                cpu = line_match.group(1)
+                if cpu == '':
+                    cpu = 'all'
+                fields = line_match.group(2).split(' ')
+                field_names = ('user', 'nice', 'system', 'idle', 'iowait', 'irq', 'softirq', 'steal', 'guest', 'guest_nice')
+                for i in range(0, len(fields)):
+                    measurement_name = "hardware.cpu.utilisation.{0}.{1}".format(cpu, field_names[i])
+                    data[measurement_name] = (float(fields[i]) / clock_ticks_per_second * 100, '%s')
+
+def data_disk_usage(data, device):
+    key = "hardware.disk.{0}".format(device['name'])
+    if device['fsuse%'] is not None:
+        data["{0}.usage".format(key)] = (float(device['fsuse%'][0:-1]), '%')
+    if device['fsavail'] is not None:
+        data["{0}.available".format(key)] = (int(device['fsavail']), 'bytes')
+    if 'children' in device:
+        for child in device['children']:
+            child_key = "{0}.partition.{1}".format(key, child['name'])
+            if child['fsuse%'] is not None:
+                data["{0}.usage".format(child_key)] = (float(child['fsuse%'][0:-1]), '%')
+            if child['fsavail'] is not None:
+                data["{0}.available".format(child_key)] = (int(child['fsavail']), 'bytes')
+
+def data_disk_smart(data, device_name):
+    key = "hardware.disk.{0}.SMART".format(device_name)
+
+    status = json.loads(cmd("smartctl -Hj {0}".format(device_name)))
+    data["{0}.passed".format(key)] = (status['smart_status']['passed'], 'bool')
+
+    details = json.loads(cmd("smartctl -Aj {0}".format(device_name)))
+    if details['device']['type'] == 'sat':
+        for attribute in details['ata_smart_attributes']['table']:
+            data["{0}.attributes.{1}".format(key, attribute['name'])] = (float(attribute['raw']['value']), 'raw')
+    elif details['device']['type'] == 'nvme':
+        for attribute, value in details['nvme_smart_health_information_log'].items():
+            data["{0}.attributes.{1}".format(key, attribute)] = (float(value), 'raw')
+
+def data_ipmi(data):
+    sdr_res = cmd("ipmitool -c sdr")
+    sensors = {}
+    for line in sdr_res.split("\n"):
+        columns = line.split(',')
+        if columns[3] not in ['ns', '0.0']:
+            key = "hardware.ipmi.{0}".format(columns[0])
+            data["{0}.value".format(key)] = (float(columns[1]), 'raw', columns[2])
+            data["{0}.ok".format(key)] = (columns[3] == 'ok', 'bool')
+
+def data_share_size(data, share):
+    result = cmd("btrfs filesystem du -s --raw {0} | tail -n+2".format(share)).split()
+    data["share.{0}.size".format(share)] = (int(result[2]), 'bytes')
+
+def data_share_lastsync(data, share):
+    result = cmd("ls -l --time-style=+'%Y-%m-%d %H:%M:%S' {0}/.lastsync | cut -d' ' -f 6,7".format(share))
+    data["share.{0}.lastsync".format(share)] = (result, 'date')
+
+
+### OTHER ###
+
+def cmd(command):
+    print("--- CMD:", command)
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout = result.stdout.decode('utf-8').rstrip("\n")
+    stderr = result.stderr.decode('utf-8').rstrip("\n")
+    print("  | OUT:", stdout)
+    print("  | ERR:", stderr)
+    if result.returncode != 0:
+        raise Exception("Command returned code {}".format(result.returncode))
+    return stdout
+
+def get_disk_devices():
+    result = cmd('lsblk -pnJb -o NAME,FSAVAIL,FSUSE%')
+    devices = json.loads(result)
+    return devices['blockdevices']
+
+
+### ENTRY POINT ###
+
+if __name__ == '__main__':
+    main()
---
+++ systemmonitor/monitorapi.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+
+from datetime import *
+import json
+import mariadb # pip3 install mariadb
+import sys
+
+
+db_user = 'monitor_select'
+db_pass = 'qwerty'
+db_host = 'henry'
+db_schema = 'monitor'
+
+datetime_format = '%Y-%m-%d %H:%M:%S'
+
+
+class MonitorApi():
+
+    def __init__(self, db_user, db_pass, db_host, db_schema):
+        self.db_user = db_user
+        self.db_pass = db_pass
+        self.db_host = db_host
+        self.db_schema = db_schema
+
+    def get(self, samples=None, cleanup=True):
+        self.connect()
+        
+        cur = self.connection.cursor()
+
+        earliest = None
+        second_earliest = None
+        if samples is not None:
+            if samples < 1:
+                raise Exception("Number of samples needs to be at least one")
+            cur.execute("SELECT distinct(taken) FROM measurements ORDER BY taken desc LIMIT ?", (samples + 1,))
+            count = 0
+            for (date,) in cur:
+                second_earliest = earliest
+                earliest = date
+                count += 1
+            if count <= samples:
+                raise Exception("There aren't enough samples in the database")
+        else:
+            raise Exception("Need to specify number of samples")
+
+        if second_earliest is None:
+            raise Exception("Not enough samples - there needs to be at least two in the database")
+
+        cur.execute("SELECT taken, measurement, value_type, value, unit FROM measurements WHERE taken >= ? OR (taken >= ? AND value_type = '%s') ORDER BY taken",
+            (second_earliest, earliest,))
+
+        data = {}
+
+        for (taken, measurement, value_type, value, unit) in cur:
+            category = measurement.split('.')
+
+            pointer = data
+            for cat in category:
+                if cat not in pointer:
+                    pointer[cat] = {}
+                pointer = pointer[cat]
+
+            pointer['type'] = value_type
+
+            if 'values' not in pointer:
+                pointer['values'] = {}
+
+            if value_type in ('%', 'raw'):
+                pointer['values'][taken] = float(value)
+
+            elif value_type == '%s':
+                if 'raw' not in pointer:
+                    pointer['raw'] = {}
+                pointer['raw'][taken] = float(value)
+                if len(pointer['raw'].keys()) > 1:
+                    previous_time = list(pointer['raw'].keys())[-2]
+                    delta_seconds = (taken - previous_time).total_seconds()
+                    pointer['values'][taken] = (float(value) - pointer['raw'][previous_time]) / delta_seconds
+
+            elif value_type in ('bytes'):
+                pointer['values'][taken] = int(value)
+
+            elif value_type == 'bool':
+                pointer['values'][taken] = (value == 'True')
+
+            elif value_type == 'date':
+                if value == '':
+                    pointer['values'][taken] = None
+                else:
+                    pointer['values'][taken] = datetime.strptime(value, datetime_format)
+
+            elif value_type == 'string':
+                pointer['values'][taken] = value
+
+            else:
+                raise Exception("Invalid type: {}".format(value_type))
+
+            if unit is not None:
+                if 'unit' in pointer and pointer['unit'] != unit:
+                    raise Exception("Varying unit not supported")
+                pointer['unit'] = unit
+        
+        self.disconnect()
+
+        if cleanup:
+            self.cleanup(data)
+
+        return data;
+
+    def cleanup(self, data):
+        if 'type' in data and isinstance(data['type'], str):
+            if 'raw' in data:
+                del data['raw']
+        else:
+            for k in data:
+                self.cleanup(data[k])
+
+    def connect(self):
+        try:
+            conn = mariadb.connect(
+                user = self.db_user,
+                password = self.db_pass,
+                host = self.db_host,
+                database = self.db_schema
+            )
+        except mariadb.Error as e:
+            print(f"Error connecting: {e}")
+            sys.exit(1)
+        self.connection = conn
+
+    def disconnect(self):
+        self.connection.close()
+
+
+class DateTimeEncoder(json.JSONEncoder):
+
+    def _preprocess_date(self, obj):
+        if isinstance(obj, (date, datetime, timedelta)):
+            return str(obj)
+        elif isinstance(obj, dict):
+            return {self._preprocess_date(k): self._preprocess_date(v) for k,v in obj.items()}
+        elif isinstance(obj, list):
+            return [self._preprocess_date(i) for i in obj]
+        return obj
+
+    def default(self, obj):
+        if isinstance(obj, (date, datetime, timedelta)):
+            return str(obj)
+        return super().default(obj)
+
+    def iterencode(self, obj, _one_shot=True):
+        return super().iterencode(self._preprocess_date(obj), _one_shot)
+
+
+if __name__ == '__main__':
+    m = MonitorApi(db_user, db_pass, db_host, db_schema)
+    samples = 1
+    if len(sys.argv) > 1:
+        samples = int(sys.argv[1])
+    res = m.get(samples=samples)
+    print(json.dumps(res, cls=DateTimeEncoder, sort_keys=True, indent=4))
