621b21cfa41bd8c3ae1c07fd1be2289f2cce850a
Jordan Leppert <jordanleppert@gmail.com>
2020-11-09 14:59:01 +0000

    Config now looked for in multiple places

--- systemmonitor/common.py
+++ systemmonitor/common.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 
 import mariadb
+import os.path
 import yaml
 
 
 def get_config(host):
-    config_file = '/usr/local/etc/systemmonitor.yml'
-    with open(config_file, 'r') as fh:
-        config = yaml.load(fh)
-    return config[host]
+    config_files = [
+        '~/.config/systemmonitor.yml',
+        '/usr/local/etc/systemmonitor.yml',
+        '/etc/systemmonitor.yml',
+    ]
+    for config_file in config_files:
+        config_file = os.path.expanduser(config_file)
+        try:
+            with open(config_file, 'r') as fh:
+                config = yaml.load(fh)
+            return config[host]
+        except Exception:
+            pass
+    raise Exception("Did not find valid config file in: {}".format(', '.join(config_files)))
