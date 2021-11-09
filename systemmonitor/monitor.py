f187a3000350e9178582ff6255f15a29069ac162
Jordan Leppert <jordanleppert@gmail.com>
2021-11-09 14:56:58 +0000

    Added sensors data

--- systemmonitor/monitor.py
+++ systemmonitor/monitor.py
@@ -10,6 +10,9 @@ import subprocess
 import sys
 
 
+sensor_regex = re.compile(r'(\D+)(\d+)_(.+)')
+
+
 class CommandException(Exception):
 
     def __init__(self, code, error):
@@ -79,6 +82,9 @@ def main():
 
     # IPMI Info
     data_ipmi(data)
+
+    # Sensor info
+    data_sensors(data)
 
 
     ### CUSTOM ###
@@ -226,6 +232,46 @@ def data_ipmi(data):
     except CommandException as e:
         err("IPMI command failed:", e.error)
 
+def data_sensors(data):
+    try:
+        boundaries = ['min', 'input', 'max', 'crit']
+        sensors = json.loads(cmd("sensors -j"))
+        sensor_data = {}
+        for module in sensors:
+            sensor_data[module] = {}
+            for sensor in sensors[module]:
+                if sensor == 'Adapter':
+                    continue
+                sensor_data[module][sensor] = {}
+                for reading_k, reading_v in sensors[module][sensor].items():
+                    reading_match = sensor_regex.match(reading_k)
+                    if reading_match:
+                        reading_type = reading_match.group(1)
+                        reading_type_num = reading_match.group(2)
+                        reading_boundary = reading_match.group(3)
+                        
+                        if 'type' not in sensor_data[module][sensor]:
+                            sensor_data[module][sensor]['type'] = reading_type
+                        else:
+                            if reading_type != sensor_data[module][sensor]['type']:
+                                raise Exception("Multiple reading types in {}/{}".format(module, sensor))
+                        
+                        if reading_boundary in boundaries:
+                            sensor_data[module][sensor][reading_boundary] = reading_v
+                    else:
+                        raise Exception("Could not parse label '{}'".format(reading_k))
+        for module in sensor_data:
+            for sensor in sensor_data[module]:
+                sensor_type = sensor_data[module][sensor]['type']
+                for reading_boundary in sensor_data[module][sensor]:
+                    if reading_boundary == 'type':
+                        continue
+                    key = "hardware.sensors.{0}.{1}_{2}.{3}".format(module, sensor, sensor_type, reading_boundary)
+                    value = sensor_data[module][sensor][reading_boundary]
+                    data[key] = (float(value), 'raw')
+    except CommandException as e:
+        err("Sensors command failed:", e.error)
+    
 
 ### Custom methods ###
 
