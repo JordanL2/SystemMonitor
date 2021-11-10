eef67cabb5dea6412183b2a0d8ed8196e3ef088c
Jordan Leppert <jordanleppert@gmail.com>
2021-11-10 20:49:45 +0000

    Adding btrfs filesystem missing devices boolean

--- systemmonitor/monitor.py
+++ systemmonitor/monitor.py
@@ -13,6 +13,7 @@ import sys
 sensor_regex = re.compile(r'(\D+)(\d*)_(.+)')
 btrfs_filesystem_regex = re.compile(r'Label:\s*(\S+)\s+uuid:\s+(\S+)')
 btrfs_device_regex = re.compile(r'\s*devid\s+(\S+)\s+size\s+(\S+)\s+used\s+(\S+)\s+path\s+(\S+)')
+btrfs_device_missing_regex = re.compile(r'\s*\*\*\*\s*Some devices missing\s*')
 
 
 class CommandException(Exception):
@@ -109,9 +110,8 @@ def get(local_config=None):
         data_disk_smart(data, device['name'])
     
     # Btrfs device stats
-    btrfs_devices = get_btrfs_devices(disk_devices)
-    for device in btrfs_devices:
-        data_btrfs_device_stats(data, device)
+    btrfs_filesystems = get_btrfs_devices(disk_devices)
+    data_btrfs_device_stats(data, btrfs_filesystems)
 
     # IPMI Info
     data_ipmi(data)
@@ -202,17 +202,20 @@ def data_disk_smart(data, device_name):
     except CommandException as e:
         err("SMART command failed:", e.error)
 
-def data_btrfs_device_stats(data, device):
-    try:
-        out = cmd("sudo btrfs device stats {}".format(device['device']))
-        for line in out.split("\n"):
-            row = line.split()
-            measure = row[0].split('.')[1]
-            count = int(row[1])
-            key = "btrfs.filesystem.{0}.device.{1}.stats.{2}".format(device['filesystem'], device['device'], measure)
-            data[key] = (float(count), 'raw')
-    except CommandException as e:
-        err("btrfs command failed:", e.error)
+def data_btrfs_device_stats(data, filesystems):
+    for filesystem in filesystems:
+        for device in filesystems[filesystem]['devices']:
+            data["btrfs.filesystem.{0}.devices_missing".format(filesystem)] = (filesystems[filesystem]['devices_missing'], 'bool')
+            try:
+                out = cmd("sudo btrfs device stats {}".format(device))
+                for line in out.split("\n"):
+                    row = line.split()
+                    measure = row[0].split('.')[1]
+                    count = int(row[1])
+                    key = "btrfs.filesystem.{0}.device.{1}.stats.{2}".format(filesystem, device, measure)
+                    data[key] = (float(count), 'raw')
+            except CommandException as e:
+                err("btrfs command failed:", e.error)
 
 def data_ipmi(data):
     try:
@@ -293,23 +296,28 @@ def get_disk_devices():
 
 def get_btrfs_devices(devices):
     try:
-        btrfs_devices = []
+        filesystems = {}
         filesystem = None
         result = cmd('sudo btrfs filesystem show')
         for line in result.split("\n"):
             regex_match = btrfs_filesystem_regex.match(line)
             if regex_match:
                 filesystem = regex_match.group(2)
+                filesystems[filesystem] = {
+                    'devices_missing': False,
+                    'devices': [],
+                }
                 continue
             regex_match = btrfs_device_regex.match(line)
             if regex_match:
                 device = regex_match.group(4)
-                btrfs_devices.append({
-                    'filesystem': filesystem,
-                    'device': device,
-                })
+                filesystems[filesystem]['devices'].append(device)
                 continue
-        return btrfs_devices
+            regex_match = btrfs_device_missing_regex.match(line)
+            if regex_match:
+                filesystems[filesystem]['devices_missing'] = True
+                continue
+        return filesystems
     except CommandException as e:
         err("btrfs command failed:", e.error)
         return []
